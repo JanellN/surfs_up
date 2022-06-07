0c76aaec2471c8a39506b4cfa861ae83480395f0
Janell Napper <janell.napper@gmail.com>
2022-06-07 10:28:16 -0400

    updating flask

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from cProfile import run
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world'
-
-set FLASK_APP =app.py
+    return 'Hello World'
